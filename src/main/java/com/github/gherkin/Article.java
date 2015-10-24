367eecfa53cc64a96c234cf0a621a13d2867dcbb
Gherkin <niyohn@stekligan.se>
2015-10-24 16:08:17 +0200

    added velocity!

---
+++ src/main/java/com/github/gherkin/Article.java
@@ -0,0 +1,7 @@
+package com.github.gherkin;
+
+/**
+ * Created by niyohn on 10/24/15.
+ */
+public class Article {
+}
--- src/main/java/com/github/gherkin/FrontServlet.java
+++ src/main/java/com/github/gherkin/FrontServlet.java
@@ -7,6 +7,7 @@ import org.apache.velocity.app.Velocity;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
+import java.util.Arrays;
 
 public class FrontServlet extends javax.servlet.http.HttpServlet {
 
@@ -14,8 +15,11 @@ public class FrontServlet extends javax.servlet.http.HttpServlet {
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, java.io.IOException {
         Template template = Velocity.getTemplate("site.vm");
         VelocityContext context = new VelocityContext();
-        context.put("name", "matte");
+
+        context.put("articles", Arrays.asList("post1", "post2", "post3"));
+
         template.merge(context, resp.getWriter());
+
 
 
     }
